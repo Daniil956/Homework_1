a5367d815bd8a54824d2091a560371f5bdab2912
Даниил <geimer_cok@mail.ru>
2022-11-27 21:07:04 +0300

    de

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Enter numbers: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double count = 2;
+while(count <= a)
+{
+Console.WriteLine(count);
+count = count + 2;
+}
