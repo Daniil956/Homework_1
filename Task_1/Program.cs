2f74f3b60a1a1332260a4fd716a9e44bbf61b431
Даниил <geimer_cok@mail.ru>
2022-11-27 19:00:35 +0300

    Первая задача созвдана

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Enter two numbers: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b )
+{
+    Console.WriteLine($"Число {a} больше, чем число {b}");
+}
+else
+{
+    Console.WriteLine($"Число {b} больше, чем число {a}");
+}
