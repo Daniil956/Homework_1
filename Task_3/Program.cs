9ff60789f2d3eb76fab41987d339413ac9961156
Даниил <geimer_cok@mail.ru>
2022-11-27 19:38:17 +0300

    Третья задача готова

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Enter numbers: ");
+double a = Convert.ToInt32(Console.ReadLine());
+if (a%2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
