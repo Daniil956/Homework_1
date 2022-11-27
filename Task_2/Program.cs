1713d2758f90cd8ea24fa4e2d5edcccd7a1f3d9b
Даниил <geimer_cok@mail.ru>
2022-11-27 19:32:37 +0300

    Вторая задача готова

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Enter three numbers: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+double q = Convert.ToInt32(Console.ReadLine());
+
+double max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max > q)
+{
+Console.WriteLine($"Самое большое число: {max}");
+}
+else 
+{
+Console.WriteLine($"Самое большое число: {q}");
+}
